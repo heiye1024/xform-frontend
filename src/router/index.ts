9cb09bedb01452eb99d8eb5a3556b7293537ca80
Aidan <aidan@AidandeMacBook-Pro.local>
2021-07-04 12:10:46 +0800

    add route

---
+++ src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/admin',
+        name: 'admin',
+        component: null
+
+    }
+]
